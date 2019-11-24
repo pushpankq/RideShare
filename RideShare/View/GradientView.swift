3ef793cbbd54b91270eba54cbde407978d680ac5
Pushpank Kumar <pushpankq@gmail.com>
2019-11-24 15:49:32 +0530

    Feature: SetUp RideShare App Add podfiles Add SwiftLint

---
+++ RideShare/View/GradientView.swift
@@ -0,0 +1,28 @@
+//
+//  GradientView.swift
+//  RideShare
+//
+//  Created by Puspank Kumar on 24/11/19.
+//  Copyright © 2019 Puspank Kumar. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    let gradient = CAGradientLayer()
+    
+    override func awakeFromNib() {
+        setUpGradientview()
+    }
+    
+    func setUpGradientview() {
+        gradient.frame = self.bounds
+        gradient.colors = [UIColor.white.cgColor, UIColor(white: 1.0, alpha: 0.0).cgColor]
+        gradient.startPoint = .zero
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        gradient.locations = [0.8, 1.0]
+        self.layer.addSublayer(gradient)
+    }
+
+}
---
+++ RideShare/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RideShare
+//
+//  Created by Puspank Kumar on 24/11/19.
+//  Copyright © 2019 Puspank Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
